dc343276b2074470df9c580a1e5ec03175e7c57f
Hayato Clarke <hayatoclarke@gmail.com>
2020-06-28 23:39:24 +0900

    added destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post '/recipes', to: 'recipes#create'
   get '/recipes/:id/edit', to: 'recipes#edit', as: :edit_recipe
   patch '/recipes/:id', to: 'recipes#update'
+  delete '/recipes/:id', to: 'recipes#destroy'
 end
